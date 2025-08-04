8f559b5eeb9f87115b8c2cdcb903158f089ddac2
Spencer Richardson <monsterchug@gmail.com>
2025-08-04 13:49:00 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   // Use VITE_BASE from env, fallback to '/' for dev and '/srich3portfolio/' for prod
-  const base = process.env.VITE_BASE || (mode === 'production' ? '/srich3portfolio/' : '/');
+  const base = '/srich3portfolio/' ;
   return {
     plugins: [react()],
     base,
