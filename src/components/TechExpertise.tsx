4442dac82832f7af30a14c972ed9175ae42bd0c8
srich3 <monsterchug@gmail.com>
2025-08-20 23:19:08 -0700

    Update skills,  fix header size

--- src/components/TechExpertise.tsx
+++ src/components/TechExpertise.tsx
@@ -119,7 +119,7 @@ const TechExpertise: React.FC = () => {
           viewport={{ once: true }}
           className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10"
         >
-          {expertiseAreas.map((area, index) => (
+          {expertiseAreas.map((area) => (
             <motion.div
               key={area.category}
               variants={cardVariants}
